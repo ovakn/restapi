cd1504d957dcb140483c771f04a3625c8143adf2
ovakn <islamnah81@gmail.com>
2024-08-02 18:48:48 +0300

    q6

--- src/main/java/com/ovakn/restapi/repository/PurchasesRep.java
+++ src/main/java/com/ovakn/restapi/repository/PurchasesRep.java
@@ -2,6 +2,8 @@ package com.ovakn.restapi.repository;
 
 import com.ovakn.restapi.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PurchasesRep extends JpaRepository<Purchase, Integer> {
 }
--- src/main/java/com/ovakn/restapi/repository/UserRep.java
+++ src/main/java/com/ovakn/restapi/repository/UserRep.java
@@ -2,8 +2,9 @@ package com.ovakn.restapi.repository;
 
 import com.ovakn.restapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRep extends JpaRepository<User, Integer> {
-    User findByName(String name);
     User findByEmail(String email);
 }
